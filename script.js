b8656f9960900d668ebfde4f3af12e035cd351c1
Frederico Batista de Lima <fredlimab@gmail.com>
2019-05-09 13:44:21 -0300

    Modified the object 'calculadora', changed the color

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-var op;
-function setOp(button){
-	op = button.value;
-
-	console.log(op);
-}
-
-function igualA(){
+Calculadora = {
+	op: "",
+	setOp: function(button){
+		this.op = button.value;
+	},
+ 	igualA: function(){
 	var a = parseInt(document.querySelector("#v1").value);
 	var b = parseInt(document.querySelector("#v2").value);
 	var calculate
-
-	if (op == "add") {
+		if (this.op == "add") {
 		calculate = a + b;
-	} else if(op == "min") {
+	} else if(this.op == "min") {
 		calculate = a - b;
-	} else if(op == "div") {
+	} else if(this.op == "div") {
 		calculate = a / b;
-	} else if(op == "mul") {
+	} else if(this.op == "mul") {
 		calculate = a * b;
 	}
 	document.querySelector('#results').innerHTML = parseInt(calculate);
 	}
+}
+
