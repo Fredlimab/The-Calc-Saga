915f164c5e66a49562dea874904739a9688da34f
Frederico Batista de Lima <fredlimab@gmail.com>
2019-05-20 18:14:26 -0300

    Now our calculator uses just one input, and the same field to show the result

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 Calculadora = {
+	valor: 0,
 	op: "",
 	setOp: function(button){
 		this.op = button.value;
+
+		if (this.valor == 0){
+			this.valor = parseInt(document.querySelector("#numero").value);
+		} else if(this.valor > 0) {
+			this.igualA()
+		}
 	},
  	igualA: function(){
-	var a = parseInt(document.querySelector("#v1").value);
-	var b = parseInt(document.querySelector("#v2").value);
-	var calculate
-		if (this.op == "add") {
-		calculate = a + b;
+ 	if (["add","min","div","mul"].indexOf(this.op) < 0){
+ 		document.querySelector('#numero').innerHTML = parseInt(document.querySelector("#numero").value);
+ 		} else {
+	var a = this.valor;
+	var b = parseInt(document.querySelector("#numero").value);
+	
+		if (this.op == "add") {	 
+		this.valor = a + b;
 	} else if(this.op == "min") {
-		calculate = a - b;
+		this.valor = a - b;
 	} else if(this.op == "div") {
-		calculate = a / b;
+		this.valor = a / b;
 	} else if(this.op == "mul") {
-		calculate = a * b;
+		this.valor = a * b;
 	}
-	document.querySelector('#results').innerHTML = parseInt(calculate);
+	document.querySelector('#numero').value = this.valor;
+	}
+	
 	}
 }
 
